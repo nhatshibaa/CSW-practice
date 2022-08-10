0e2c69567dfd061c9bbcd44b6137a183c45546dd
os_gpcp_gpdn40 <os_gpcp_gpdn40@ViettelGroup.ViettelAD.com>
2022-08-10 20:18:43 +0700

    pushh

--- src/main/java/com/example/nguyentiennhat/IProduct.java
+++ src/main/java/com/example/nguyentiennhat/IProduct.java
@@ -1,8 +1,9 @@
 package com.example.nguyentiennhat;
 
+import java.io.Serializable;
 import java.util.List;
 
-public interface IProduct {
+public interface IProduct extends Serializable {
     Product save(Product product);
 
     List<Product> findAll();
